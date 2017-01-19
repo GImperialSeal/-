0eac560c1a669c7e7e6dddbff52781c8b60e027c
顾玉玺 <imperial@guyuxideMacBook-Air.local>
2017-01-19 16:42:04 +0800

    no message

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+
+
+target ‘coolNavi’ do
+   pod 'PureLayout', '~> 3.0.2'
+pod 'AFNetworking', '~> 3.1.0’
+
+end
---
+++ Pods/Target Support Files/Pods-coolNavi/Pods-coolNavi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_coolNavi : NSObject
+@end
+@implementation PodsDummy_Pods_coolNavi
+@end
